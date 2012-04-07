0e7a9829e808da4e14b6ea96f3e9850cf1ee0803
Andrew O'Connor <andrew.oconnor@gmail.com>
2012-04-07 14:40:55 +0100

    Initial Commit

---
+++ LSystem/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  LSystem
+//
+//  Created by Andrew O'Connor on 07/04/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
