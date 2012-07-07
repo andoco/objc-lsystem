38c66cdeca9ef212000e1026f8a019c36c7784e9
Andrew O'Connor <andrew.oconnor@gmail.com>
2012-07-08 00:39:59 +0100

    Add menu selection of l-system rules to draw

--- LSystem-Cocos2d/LSystemLayer.m
+++ LSystem-Cocos2d/LSystemLayer.m
@@ -20,10 +20,16 @@
     
 }
 
+@property (nonatomic, retain) CCMenu *menu;
+@property (nonatomic, retain) LSystemNode *lsystem;
+
 @end
 
 // LSystemLayer implementation
 @implementation LSystemLayer
+
+@synthesize menu;
+@synthesize lsystem;
 
 +(CCScene *) scene
 {
@@ -43,30 +49,88 @@
 -(id) init
 {
 	if( (self=[super init])) {
-		                                
-        NSDictionary *straight = [NSDictionary dictionaryWithObjectsAndKeys:@"FFF", @"1", nil];
-        NSDictionary *bend = [NSDictionary dictionaryWithObjectsAndKeys:@"FF-1", @"1", nil];
-        NSDictionary *branches = [NSDictionary dictionaryWithObjectsAndKeys:@"FF-[1]++F+F", @"1", nil];
-        NSDictionary *moreBranches = [NSDictionary dictionaryWithObjectsAndKeys:@"FF-[1]++F+F+1", @"1", nil];
-        NSDictionary *elaborate = [NSDictionary dictionaryWithObjectsAndKeys:@"FF[-2]3[+3]", @"1", @"FF+F-F-F[FFF3][+3]-F-F3", @"2", @"FF-F+F+F[2][-2]+F+F2", @"3", nil];
+        self.isTouchEnabled = YES;
         
-        NSDictionary *gnarled = [NSDictionary dictionaryWithObjectsAndKeys:
-                                 @"FF[++FF[2][+FF2]][-FF3]", @"1",
-                                 @"F-F-F+[2]F+F+F+F+>[2]", @"2",
-                                 @"F+F+F-[2]F-F-F-F->[2]", @"3",
-                                 nil];
-        
-        NSDictionary *crooked = [NSDictionary dictionaryWithObjectsAndKeys:@"F-F+2", @"1", @"F-[[-F-F+F+FF2]+FF2]+F[+F+F+FF2]-FF+F-F2", @"2", nil];
-        
-        LSystemNode *lsys = [LSystemNode lsystemWithRules:moreBranches];
-        [self addChild:lsys];        
+        [self showMenu];
 	}
 	return self;
+}
+
+-(void) registerWithTouchDispatcher {
+    [[CCTouchDispatcher sharedDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
 }
 
 // on "dealloc" you need to release all your retained objects
 - (void) dealloc
 {
+    [menu release];
 	[super dealloc];
 }
+
+-(NSDictionary*) systemRules {
+    NSDictionary *straight = [NSDictionary dictionaryWithObjectsAndKeys:@"FFF", @"1", nil];
+    NSDictionary *bend = [NSDictionary dictionaryWithObjectsAndKeys:@"FF-1", @"1", nil];
+    NSDictionary *branches = [NSDictionary dictionaryWithObjectsAndKeys:@"FF-[1]++F+F", @"1", nil];
+    NSDictionary *moreBranches = [NSDictionary dictionaryWithObjectsAndKeys:@"FF-[1]++F+F+1", @"1", nil];
+    NSDictionary *elaborate = [NSDictionary dictionaryWithObjectsAndKeys:@"FF[-2]3[+3]", @"1", @"FF+F-F-F[FFF3][+3]-F-F3", @"2", @"FF-F+F+F[2][-2]+F+F2", @"3", nil];
+    
+    NSDictionary *gnarled = [NSDictionary dictionaryWithObjectsAndKeys:
+                             @"FF[++FF[2][+FF2]][-FF3]", @"1",
+                             @"F-F-F+[2]F+F+F+F+>[2]", @"2",
+                             @"F+F+F-[2]F-F-F-F->[2]", @"3",
+                             nil];
+    
+    NSDictionary *crooked = [NSDictionary dictionaryWithObjectsAndKeys:@"F-F+2", @"1", @"F-[[-F-F+F+FF2]+FF2]+F[+F+F+FF2]-FF+F-F2", @"2", nil];
+    
+    
+    NSDictionary *rules = [NSDictionary dictionaryWithObjectsAndKeys:
+                           straight, @"Straight", 
+                           bend, @"Bend",
+                           branches, @"Branches",
+                           moreBranches, @"More Branches",
+                           elaborate, @"Elaborate",
+                           gnarled, @"Gnarled",
+                           crooked, @"Crooked",
+                           nil];
+
+    return rules;
+}
+
+-(void) showMenu {
+    if (!menu) {
+        self.menu = [CCMenu menuWithItems:nil];
+        
+        NSDictionary *sysRules = [self systemRules];
+        
+        for (NSString *ruleKey in sysRules.allKeys) {
+            CCMenuItemFont *item = [CCMenuItemFont itemFromString:ruleKey block:^(id sender) {
+                NSDictionary *rules = [sysRules objectForKey:ruleKey];
+                menu.visible = NO;
+                [self showLSystemWithRules:rules];
+            }];
+            [menu addChild:item];
+        }
+        
+        [menu alignItemsVertically];
+        
+        [self addChild:menu z:2];        
+    }
+    
+    menu.visible = YES;
+}
+
+-(void) showLSystemWithRules:(NSDictionary*)rules {
+    // remove current lsystem
+    [lsystem removeFromParentAndCleanup:YES];
+    
+    self.lsystem = [LSystemNode lsystemWithRules:rules];
+    [self addChild:lsystem];
+}
+
+-(BOOL) ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self showMenu];
+    
+    return YES;
+}
+
 @end
